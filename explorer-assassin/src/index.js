1e7fb7d54af312e5ae49f191f575540382bfd9f8
Tengis B <tengis617@users.noreply.github.com>
2018-03-27 15:52:26 +0700

    inital explorer assassin

--- explorer-assassin/src/index.js
+++ explorer-assassin/src/index.js
@@ -1,6 +1,15 @@
 
 const sumDigits = n => n.toString().split('').reduce((acc, x) => acc + Number(x), 0)
 
+const findMaxConsecutive = n => {
+  // finds the maximum number where x + 1 is less than n
+  const numberOfNines = Math.floor(n / 9)
+  const firstDigit = n % 9
+  const max = Number(`${firstDigit + 1}${String(9).repeat(numberOfNines)}`)
+  return max - 1
+}
+
 module.exports = {
-  sumDigits
+  sumDigits,
+  findMaxConsecutive
 }
--- explorer-assassin/src/index.test.js
+++ explorer-assassin/src/index.test.js
@@ -1,5 +1,5 @@
 const test = require('tape')
-const { sumDigits } = require('./index')
+const { sumDigits, findMaxConsecutive } = require('./index')
 
 test('sumDigits test', (t) => {
   t.plan(5)
@@ -8,4 +8,12 @@ test('sumDigits test', (t) => {
   t.equal(sumDigits(10000000000000), 1, 'should handle large numbers')
   t.equal(sumDigits(12345), 15, 'should return 15 when input is 12345')
   t.equal(sumDigits(0), 0, 'should return 0 if input is 0')
+})
+test('findMaxConsecutive test', (t) => {
+  t.plan(5)
+  t.equal(findMaxConsecutive(22), 598)
+  t.equal(findMaxConsecutive(21), 498)
+  t.equal(findMaxConsecutive(11), 38)
+  t.equal(findMaxConsecutive(1), 1)
+  t.equal(findMaxConsecutive(10), 28)
 })
